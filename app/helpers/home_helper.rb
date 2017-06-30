9ac7b785a6c536a5220fd1d928db1ed3144c1199
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2017-06-30 21:44:50 +0900

    レイアウト

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,16 +3,10 @@ module HomeHelper
     return nil unless product
 
     content_tag(:div, class: "col-xs-4") do
-      content_tag(:div, class: "product") do
-        img = content_tag(:div, class: "product__image") do
-          link_to(
-            image_tag(image_product_path(product), alt: product.name, size: "200x200"),
-            product)
-        end
-        link = content_tag(:div, class: "product__name") do
-          link_to(product.name, product)
-        end
-        img + link
+      content_tag(:a, href: product_path(product), class: "thumbnail product") do
+        img = image_tag(image_product_path(product), alt: product.name, class: "product__image")
+        name = content_tag(:span, product.name)
+        img + name
       end
     end
   end
