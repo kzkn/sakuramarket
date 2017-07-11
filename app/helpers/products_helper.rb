0ccdc83a563b591d1c7d6a337e471bdf51e4c54e
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2017-07-11 23:39:41 +0900

    products_helper, users_helper を削除

--- app/helpers/products_helper.rb
+++
@@ -1,6 +0,0 @@
-module ProductsHelper
-  def product_image_tag(product, size)
-    # TODO もう少し使いやすくする
-    image_tag product_image_path(product), alt: product.name, size: "#{size}x#{size}"
-  end
-end
