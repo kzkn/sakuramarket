97ad36a093a09d5a2da1d473fc9a79f553a6c6a3
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2017-07-20 22:34:01 +0900

    管理メニュー、管理画面の日本語化

---
+++ app/controllers/admin/home_controller.rb
@@ -0,0 +1,4 @@
+class Admin::HomeController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get "/admin", to: "admin/home#show"
   namespace :admin do
     resources :users, only: %i(index show edit update destroy)
     resources :products, only: %i(index show new edit create update)
