824aa36ffa2b8cf0c90c9da6831105ac4187465f
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2017-08-11 12:35:26 +0900

    トップページの feature spec

--- spec/features/roots_spec.rb
+++ spec/features/roots_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.feature "Roots", type: :feature do
-  it "list all visible products" do
-    create(:product)
-    create(:product2)
+  let!(:p1) { create(:product) }
+  let!(:p2) { create(:product2) }
 
+  it "list all visible products" do
     visit("/")
     expect(page).to have_content("p1")
     expect(page).to have_content("p2")
+  end
+
+  it "shows detail of product" do
+    visit("/")
+    click_link("p1")
+    expect(page).to have_current_path(product_path(p1))
   end
 end
