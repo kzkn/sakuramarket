d00aa9070a86873564deabbec2a4372edac5befb
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2017-08-29 21:23:16 +0900

    labeled_yen を helper から partial へ
    html_safe を使わない実装に変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,4 @@ module ApplicationHelper
   def checkmark(value)
     value ? '&#10003;'.html_safe : ''
   end
-
-  def labeled_yen(label, value)
-    "#{label} <strong>#{number_to_currency(value)}</strong>".html_safe
-  end
 end
